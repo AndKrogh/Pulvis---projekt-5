00d4bf041a4d4cb4e553f9d4d14c047955c30049
Anne-Sofie <95890841+Fidolicious@users.noreply.github.com>
2022-04-25 11:04:51 +0200

    Update boeger-slider.js

--- script/boeger-slider.js
+++ script/boeger-slider.js
@@ -12,6 +12,8 @@ wrapper[0].addEventListener("scroll", function (event) {
   scroll = wrapper[0].scrollLeft;
 });
 
+
+
 function clicked () {
   scroll = scroll += 500;
   wrapper[0].scrollTo({
@@ -19,6 +21,8 @@ function clicked () {
     behavior: 'smooth'
   });
 }
+
+
 
 function clicked2 () {
   scroll = scroll -= 500;
@@ -29,3 +33,41 @@ function clicked2 () {
   scroll = wrapper[0].scrollLeft + 50;
   console.log(wrapper[0].scrollLeft);
 }
+
+
+
+
+// ----------Eksempel på loop-------------- //
+//if (n > slides.length) {slideIndex = 1}
+    //if (n < 1) {slideIndex = slides.length}
+    //for (i = 0; i < slides.length; i++) {
+      //slides[i].style.display = "none";
+    //}
+    //for (i = 0; i < dots.length; i++) {
+      //dots[i].className = dots[i].className.replace(" active", "");
+    //}
+  //slides[slideIndex-1].style.display = "block";
+  //dots[slideIndex-1].className += " active";
+//}
+
+
+// ----------Eksempel på Kontrolstruktur-------------- //
+// var x = 15;
+// var y = 10;
+  // if (x > y)
+    // allert("X er størst");
+  //else
+    //alert("Y er størst");
+
+
+// ----------Eksempel på Array-------------- //
+//var colors = ["blue", "red", "green"];
+//function changeColor(element, color) {
+  //element.style.backgroundColor = color;
+//}
+//function printColors() {
+  //var p = document.getElementById('colorsParagraph');
+
+  //for (i = 0; i < colors.length; i++)
+    //p.innerHTML += colors[i] + ", ";
+//}
